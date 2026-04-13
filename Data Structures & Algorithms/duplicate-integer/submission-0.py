7d27bafb5e922f66f5cb3431f2e6aafc562f199b
Hussein Mourad <38833034+hussein-mourad@users.noreply.github.com>
2026-04-13 03:31:46 +0200

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        lookup = {}
+        for i in nums:
+            if i in lookup:
+                return True
+            lookup[i] = 1
+        return False
