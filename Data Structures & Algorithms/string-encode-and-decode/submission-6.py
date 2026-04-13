976e03e3951cdeeab662056ef7b82dc52cbe5f6e
Hussein Mourad <38833034+hussein-mourad@users.noreply.github.com>
2026-04-13 06:01:22 +0200

    Add: string-encode-and-decode - submission-6

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-6.py
@@ -0,0 +1,12 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        elem = "$%&".join(strs)
+        return elem + "*****" + str(len(strs))
+
+    def decode(self, s: str) -> List[str]:
+        n = int(s.split("*****")[1])
+        elems = s.split("*****")[0].split('$%&')
+        if not n:
+            return []
+        return elems
