0064e5b9a6e83f67b5132d09ab4d707a646f62cb
Hussein Mourad <38833034+hussein-mourad@users.noreply.github.com>
2026-04-14 05:51:36 +0200

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        brackets = {'(':')', '{':'}','[':']'}
+        openBrackets = ['(','{','[']
+        closeBrackets = [')','}',']']
+        stack = []
+        for c in s:
+            if c in openBrackets:
+                stack.append(c)
+            elif c in closeBrackets:
+                openB = stack.pop()
+                if brackets[openB] != c:
+                    return False
+        if len(stack):
+            return False
+        return True
+
